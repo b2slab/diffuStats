7f785db5e4ae001641647a16afb85e05da3ab842
SergiPicart <elpicart@gmail.com>
2021-02-20 13:27:41 +0000

    fixed bug in Rcpp code (arma deprecation arma::memory::acquire_chunked)

--- src/diffuse_parallel.cpp
+++ src/diffuse_parallel.cpp
@@ -16,31 +16,16 @@ using namespace Rcpp; using namespace arma;
 // [[Rcpp::export]]
 arma::sp_mat convertSparse(S4 mat) {// slight improvement with two non-nested loops
 
+  // obtain dim, i, p. x from S4 object
   IntegerVector dims = mat.slot("Dim");
   arma::urowvec i = Rcpp::as<arma::urowvec>(mat.slot("i"));
   arma::urowvec p = Rcpp::as<arma::urowvec>(mat.slot("p"));
   arma::vec x     = Rcpp::as<arma::vec>(mat.slot("x"));
-
+  
   int nrow = dims[0], ncol = dims[1];
-  arma::sp_mat res(nrow, ncol);
-
-  // create space for values, and copy
-  arma::access::rw(res.values) = arma::memory::acquire_chunked<double>(x.size() + 1);
-  arma::arrayops::copy(arma::access::rwp(res.values), x.begin(), x.size() + 1);
-
-  // create space for row_indices, and copy
-  arma::access::rw(res.row_indices) = arma::memory::acquire_chunked<arma::uword>(i.size() + 1);
-  arma::arrayops::copy(arma::access::rwp(res.row_indices), i.begin(), i.size() + 1);
-
-  // create space for col_ptrs, and copy
-  arma::access::rw(res.col_ptrs) = arma::memory::acquire<arma::uword>(p.size() + 2);
-  arma::arrayops::copy(arma::access::rwp(res.col_ptrs), p.begin(), p.size() + 1);
-
-  // important: set the sentinel as well
-  arma::access::rwp(res.col_ptrs)[p.size()+1] = std::numeric_limits<arma::uword>::max();
-
-  // set the number of non-zero elements
-  arma::access::rw(res.n_nonzero) = x.size();
+  
+  // use Armadillo sparse matrix constructor
+  arma::sp_mat res(i, p, x, nrow, ncol);
 
   // Rcout << "SpMat res:\n" << res << std::endl;
   return(res);
